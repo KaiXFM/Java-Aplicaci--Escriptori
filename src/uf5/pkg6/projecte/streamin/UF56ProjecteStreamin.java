eea4a4c0dbe49b77aa3542060d6870fa607f7e15
arnba <arnba@DESKTOP-OMLDGQJ>
2023-05-19 16:33:46 +0200

    Començament de connexió BD

---
+++ src/uf5/pkg6/projecte/streamin/UF56ProjecteStreamin.java
@@ -0,0 +1,23 @@
+package uf5.pkg6.projecte.streamin;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import uf5.pkg6.projecte.basedades.ConexioBDSingleton;
+
+public class UF56ProjecteStreamin extends Application{
+
+    public static void main(String[] args){
+        
+        ConexioBDSingleton.getConnection();
+        
+        
+        Application.launch(args);
+        
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
