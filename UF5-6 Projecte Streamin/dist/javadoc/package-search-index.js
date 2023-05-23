97773db94db77b09a26e3f2ad669e9a2171c418e
KaiXFM <102524875+KaiXFM@users.noreply.github.com>
2023-05-23 11:31:13 +0200

    Add files via upload

---
+++ UF5-6 Projecte Streamin/dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"uf5.pkg6.projecte.basedades"},{"l":"uf5.pkg6.projecte.streamin"},{"l":"uf5.pkg6.projecte.streamin.model"},{"l":"uf5.pkg6.projecte.streamin.vista"}];updateSearchResults();
