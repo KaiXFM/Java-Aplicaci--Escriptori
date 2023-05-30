586c1f913458dc5b8070670b8f478bed59fa82f9
KaiXFM <102524875+KaiXFM@users.noreply.github.com>
2023-05-30 21:21:06 +0200

    Delete UF5-6 Projecte Streamin directory

--- UF5-6 Projecte Streamin/dist/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"uf5.pkg6.projecte.basedades"},{"l":"uf5.pkg6.projecte.streamin"},{"l":"uf5.pkg6.projecte.streamin.model"},{"l":"uf5.pkg6.projecte.streamin.vista"}];updateSearchResults();
